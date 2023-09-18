be5e0e5840d33a4eda79a2142f387bdbe1517087
micah.shi <micah.shi@gmail.com>
2023-09-18 15:42:56 +0800

    二分查找

---
+++ Leetcode2023/leetcode/editor/cn/no034/no034_find_first_and_last_position_of_element_in_sorted_array.go
@@ -0,0 +1,112 @@
+package main
+
+import "fmt"
+
+/***
+*
+* 2023-09-18 15:34:01
+*
+***/
+
+//给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+//
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
+//
+// 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [5,7,7,8,8,10], target = 8
+//输出：[3,4]
+//
+// 示例 2：
+//
+//
+//输入：nums = [5,7,7,8,8,10], target = 6
+//输出：[-1,-1]
+//
+// 示例 3：
+//
+//
+//输入：nums = [], target = 0
+//输出：[-1,-1]
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= nums.length <= 10⁵
+// -10⁹ <= nums[i] <= 10⁹
+// nums 是一个非递减数组
+// -10⁹ <= target <= 10⁹
+//
+//
+// Related Topics数组 | 二分查找
+//
+// 👍 2471, 👎 0bug 反馈 | 使用指南 | 更多配套插件
+//
+//
+//
+//
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func searchRange(nums []int, target int) []int {
+	if len(nums) == 0 {
+		return []int{-1, -1}
+	}
+
+	left := leftSearch(nums, target)
+	right := rightSearch(nums, target)
+	if left == -1 {
+		return []int{-1, -1}
+	}
+	return []int{left, right}
+}
+
+func leftSearch(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+	if left >= len(nums) || nums[left] != target {
+		return -1
+	}
+	return left
+}
+func rightSearch(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			left = mid + 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+	if right < 0 || nums[right] != target {
+		return -1
+	}
+	return right
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func main() {
+	value := searchRange([]int{5, 7, 7, 8, 8, 10}, 6)
+	fmt.Println(fmt.Sprintf("%+v", value))
+}
